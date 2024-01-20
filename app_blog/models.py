7e90fa156c6b5965d9296238d33427eca4b0aad3
Piotr Górski <piotrkropagorski@gmail.com>
2024-01-20 18:24:36 +0100

    created views, url and templates

--- app_blog/models.py
+++ app_blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 # Create your models here.
 class Post(models.Model):
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    # try:
+    #     post = Post.objects.get(id=pk)
+    # except Post.DoesNotExist:
+    #     raise Http404("Nie znaleziono posta.")
+    
+    post = get_object_or_404(Post, id=pk, status=Post.Status.PUBLISHED)
+    
+    return render(request, 'blog/post/detail.html', {'post': post})
