cb62680e213cbc260a221bdc53585982036fa3b8
Piotr Górski <piotrkropagorski@gmail.com>
2024-02-03 11:45:07 +0100

    created comment model

--- backend/settings.py
+++ backend/settings.py
@@ -115,6 +115,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_USER = 'piotrkropagorski@gmail.com'
-# EMAIL_HOST_PASSWORD = ''
+# EMAIL_HOST_PASSWORD = 'vdvk dara qzem xzql'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
