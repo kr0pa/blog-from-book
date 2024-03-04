0a69b13dba48a5327d65012b2fa986cf03e81859
Piotr Górski <piotrkropagorski@gmail.com>
2024-03-04 06:46:26 +0100

    added site's map

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from app_blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('app_blog.urls', namespace='app_blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
